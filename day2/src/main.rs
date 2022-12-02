0a775887434cac6deea720b17a0d9899a1e3bf35
Bijan Riesenberg <B.Riesenberg@gmx.de>
2022-12-02 17:29:55 +0100

    day2 part2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -17,7 +17,7 @@ fn score(a: &str, b: &str) -> i32 {
     }
     if a == "A" && b == "Z" || a == "B" && b == "X" || a == "C" && b == "Y" {
         return 0;
-    } 
+    }
     6
 }
 
@@ -31,11 +31,46 @@ fn part1(input: &Vec<String>) -> i32 {
             "Z" => result += 3 + score(parts[0], parts[1]),
             _ => panic!("Unknown operation"),
         }
-        println!("{}: {}", line, result);
     }
     result
 }
 
+fn choose_win(a: &str) -> i32 {
+    match a {
+        "A" => 2,
+        "B" => 3,
+        "C" => 1,
+        _ => panic!("Unknown operation"),
+    }
+}
+fn choose_draw(a: &str) -> i32 {
+    match a {
+        "A" => 1,
+        "B" => 2,
+        "C" => 3,
+        _ => panic!("Unknown operation"),
+    }
+}
+
+fn choose_loss(a: &str) -> i32 {
+    match a {
+        "A" => 3,
+        "B" => 1,
+        "C" => 2,
+        _ => panic!("Unknown operation"),
+    }
+}
+
 fn part2(input: &Vec<String>) -> i32 {
-    0
+    let mut result = 0;
+    for line in input {
+        let parts = line.split(" ").collect::<Vec<&str>>();
+        match parts[1] {
+            "X" => result += 0 + choose_loss(parts[0]),
+            "Y" => result += 3 + choose_draw(parts[0]),
+            "Z" => result += 6 + choose_win(parts[0]),
+            _ => panic!("Unknown operation"),
+        }
+    }
+    result
 }
