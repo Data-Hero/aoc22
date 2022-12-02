d25d001d00a25ea7e19c0befa1f6252c9c2cfa4d
Bijan Riesenberg <B.Riesenberg@gmx.de>
2022-12-02 17:19:29 +0100

    day2 part1

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -11,10 +11,31 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
-fn part1(input: &Vec<String>) -> i64 {
-    0
+fn score(a: &str, b: &str) -> i32 {
+    if a == "A" && b == "X" || a == "B" && b == "Y" || a == "C" && b == "Z" {
+        return 3;
+    }
+    if a == "A" && b == "Z" || a == "B" && b == "X" || a == "C" && b == "Y" {
+        return 0;
+    } 
+    6
 }
 
-fn part2(input: &Vec<String>) -> i64 {
+fn part1(input: &Vec<String>) -> i32 {
+    let mut result = 0;
+    for line in input {
+        let parts = line.split(" ").collect::<Vec<&str>>();
+        match parts[1] {
+            "X" => result += 1 + score(parts[0], parts[1]),
+            "Y" => result += 2 + score(parts[0], parts[1]),
+            "Z" => result += 3 + score(parts[0], parts[1]),
+            _ => panic!("Unknown operation"),
+        }
+        println!("{}: {}", line, result);
+    }
+    result
+}
+
+fn part2(input: &Vec<String>) -> i32 {
     0
 }
