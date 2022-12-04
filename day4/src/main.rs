60b18db2ae35d6be756d14596394213573013c15
Bijan Riesenberg <B.Riesenberg@gmx.de>
2022-12-04 12:35:44 +0100

    day4 part1

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -11,8 +11,25 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
+fn contain(s1: &str, s2: &str) -> bool {
+    let n1 = s1.split("-").map(|x| x.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+    let n2 = s2.split("-").map(|x| x.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+    if n1[0] <= n2[0] && n1[1] >= n2[1] {
+        return true;
+    }
+    false
+}
+
 fn part1(input: &Vec<String>) -> i32 {
-    0
+    let mut result = 0;
+    for l in input {
+        let mut parts = l.split(",").collect::<Vec<&str>>();
+        if contain(parts[0],parts[1]) || contain(parts[1], parts[0]) {
+            println!("{} contains {}", parts[0], parts[1]);
+            result += 1;
+        }
+    }
+    result
 }
 
 fn part2(input: &Vec<String>) -> i32 {
