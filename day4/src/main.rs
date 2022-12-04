2023065c2ec8c6b1886373c8a1ed0b1cdd1ac9dd
Bijan Riesenberg <B.Riesenberg@gmx.de>
2022-12-04 12:17:47 +0100

    day4 init

---
+++ day4/src/main.rs
@@ -0,0 +1,20 @@
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
+
+fn main() -> io::Result<()> {
+    let file = File::open("input")?;
+    let reader = BufReader::new(file);
+    let input = reader.lines().map(|l| l.unwrap()).collect::<Vec<String>>();
+    println!("Input: {:?}", input);
+    println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
+    Ok(())
+}
+
+fn part1(input: &Vec<String>) -> i32 {
+    0
+}
+
+fn part2(input: &Vec<String>) -> i32 {
+    0
+}
