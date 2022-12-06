a23b3493ee56fc0607c2eadc53f84bbce23ea19b
Bijan Riesenberg <B.Riesenberg@gmx.de>
2022-12-06 11:18:41 +0100

    day6 part1

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -11,10 +11,30 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
-fn part1(input: &Vec<String>) -> i64 {
-    0
+fn alldiff(a: char, b: char, c: char, d: char) -> bool {
+    a != b && a != c && a != d && b != c && b != d && c != d
 }
 
-fn part2(input: &Vec<String>) -> i64 {
+fn part1(input: &Vec<String>) -> usize {
+    let count = 0;
+    let chars: Vec<char> = input[0].chars().collect();
+    for (index, character) in chars.into_iter().enumerate() {
+        if index < 4 {
+            continue;
+        } 
+        let chars_inner: Vec<char> = input[0].chars().collect();
+        let a = chars_inner[index - 4];
+        let b = chars_inner[index - 3];
+        let c = chars_inner[index - 2];
+        let d = chars_inner[index - 1];
+        println!("{}{}{}{}{}", a, b, c, d, character);
+        if alldiff(a,b,c,d) {
+            return index;
+        }
+    }
+    usize::MAX
+}
+
+fn part2(input: &Vec<String>) -> i32 {
     0
 }
