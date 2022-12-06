009964e6fe114e0d44062dfe6e92a9dd925a616a
Bijan Riesenberg <B.Riesenberg@gmx.de>
2022-12-06 11:27:43 +0100

    day6 part 2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -15,26 +15,53 @@ fn alldiff(a: char, b: char, c: char, d: char) -> bool {
     a != b && a != c && a != d && b != c && b != d && c != d
 }
 
+fn vec_alldiff(chars: &Vec<char>) -> bool {
+    for i in 0..chars.len() {
+        for j in i + 1..chars.len() {
+            if chars[i] == chars[j] {
+                return false;
+            }
+        }
+    }
+    true
+}
+
 fn part1(input: &Vec<String>) -> usize {
-    let count = 0;
     let chars: Vec<char> = input[0].chars().collect();
     for (index, character) in chars.into_iter().enumerate() {
         if index < 4 {
             continue;
-        } 
+        }
         let chars_inner: Vec<char> = input[0].chars().collect();
         let a = chars_inner[index - 4];
         let b = chars_inner[index - 3];
         let c = chars_inner[index - 2];
         let d = chars_inner[index - 1];
-        println!("{}{}{}{}{}", a, b, c, d, character);
-        if alldiff(a,b,c,d) {
+        if alldiff(a, b, c, d) {
+            println!("{}{}{}{}", a, b, c, d);
+            println!("marker: {}", character);
             return index;
         }
     }
     usize::MAX
 }
 
-fn part2(input: &Vec<String>) -> i32 {
-    0
+fn part2(input: &Vec<String>) -> usize {
+    let chars: Vec<char> = input[0].chars().collect();
+    for (index, character) in chars.into_iter().enumerate() {
+        if index < 14 {
+            continue;
+        }
+        let chars_inner: Vec<char> = input[0].chars().collect();
+        let mut marker = Vec::<char>::with_capacity(14);
+        for j in 1..15 {
+            marker.push(chars_inner[index - j]);
+        }
+        if vec_alldiff(&marker) {
+            println!("marker: {:?}", marker);
+            println!("marker: {}", character);
+            return index;
+        }
+    }
+    usize::MAX
 }
