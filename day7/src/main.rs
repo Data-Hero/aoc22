6c94c81b27c2c0d49bdf5d41af2b0311bc9a5b8a
Bijan Riesenberg <B.Riesenberg@gmx.de>
2022-12-08 00:01:16 +0100

    day7 part 1

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 
@@ -22,38 +22,22 @@ struct Node {
 fn part1(input: &Vec<String>) -> u64 {
     let mut result = 0u64;
 
-    let mut size = 0u64;
-
-    let mut folders = HashMap::<String, Node>::new();
-    let mut current_node: &str = "";
-    let mut pname = "".to_owned();
-    let mut count = 0;
+    let mut folders = BTreeMap::<String, Node>::new();
+    let mut current_path = Vec::<String>::new();
     for line in input {
-        count = count + 1;
-        if line.contains("$ cd ..") && "/" != current_node {
-            let parent_node = folders.get(current_node).unwrap();
-            pname = parent_node.name.to_owned();
-            current_node = &pname;
-            size = 0;
+        if line.contains("$ cd ..") && current_path.len() > 1 {
+           current_path.pop();
         } else if line.contains("$ cd") {
             let name = line.replace("$ cd ", "");
+            current_path.push(name.to_string());
             if !folders.contains_key(&name) {
                 let new_node = Node {
+                    name: current_path.join("/"),
                     size: 0,
-                    uid: count.to_string() + "-" + &name,
-                    name: name.to_owned(),
-                    parent: current_node.to_owned(),
-                    children: Vec::new(),
                 };
-                folders.insert(name, new_node);
+                folders.insert(current_path.join("/"), new_node);
             }
-            pname = line.replace("$ cd ", "");
-            current_node = &pname;
-            size = 0;
-        } else if line.contains("$ ls") {
-            size = 0;
-            continue;
-        } else if line.contains(&['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'])
+        }  else if line.contains(&['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'])
             && !line.contains("$")
         {
             let additional_size = line
@@ -62,78 +46,28 @@ fn part1(input: &Vec<String>) -> u64 {
                 .unwrap()
                 .parse::<u64>()
                 .unwrap();
-
-            let cn = folders.get(current_node).unwrap();
+            let cn = folders.get(&current_path.join("/")).unwrap();
             folders.insert(
-                current_node.to_string(),
+                current_path.join("/"),
                 Node {
-                    size: cn.size + additional_size,
-                    uid: cn.uid.to_string(),
                     name: cn.name.to_string(),
-                    parent: cn.parent.to_string(),
-                    children: cn.children.to_vec(),
+                    size: cn.size + additional_size,
                 },
             );
-        } else if line.contains("dir ") {
-            let name = line.split_whitespace().nth(1).unwrap();
-            let mcn = folders.get(current_node);
-            if mcn.is_some() {
-                let cn = mcn.unwrap();
-                let mut childs = cn.children.to_vec();
-                childs.push(name.to_string());
-                folders.insert(
-                    current_node.to_string(),
-                    Node {
-                        size: cn.size,
-                        uid: cn.uid.to_string(),
-                        name: cn.name.to_string(),
-                        parent: cn.parent.to_string(),
-                        children: childs,
-                    },
-                );
-            } else {
-                folders.insert(
-                    current_node.to_string(),
-                    Node {
-                        size: 0,
-                        uid: count.to_string() + "-" + name,
-                        name: name.to_owned(),
-                        parent: current_node.to_owned(),
-                        children: Vec::new(),
-                    },
-                );
-            }
         }
     }
-    println!("=====================================");
-    println!("Root: {:?}", folders.get("/").unwrap());
+
     let keys = folders.keys();
     for key in keys {
-        println!("key: {}", key); 
-        
         let val = folders.get(key).unwrap();
-        println!("childs: {:?}", val.children); 
-        let mut size = val.size;
-        
-        let mut children_list = Vec::<String>::new();
-        for child in val.children.iter() {
-            let child_node = folders.get(child).unwrap();
-            children_list.push(child_node.name.to_string());
-        }
-        while children_list.len() > 0 {
-            println!("children_list of {}: {:?}", key, children_list);
-            let child_node_string = children_list.pop().unwrap();
-            let child_node = folders.get(&child_node_string).unwrap();
-            size = size + child_node.size;
-            for child in child_node.children.iter() {
-                let cn = folders.get(child).unwrap();
-                if !children_list.contains(&cn.name) {
-                    children_list.push(cn.name.to_string());
-                }
+        let mut s = val.size; 
+        for (key2, val2) in &folders {
+            if key != key2 && key2.contains(key) {
+                s = s + folders.get(key2).unwrap().size;
             }
         }
-        if size <= 100000 {
-            result = result + size;
+        if s <= 100000 {
+            result = result + s;
         }
     }
     result
