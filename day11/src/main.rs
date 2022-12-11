fb03a28336507e22d891aead647ce490433cbf27
Bijan Riesenberg <B.Riesenberg@gmx.de>
2022-12-11 20:00:31 +0100

    day11 part2

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -2,7 +2,8 @@
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 
-const NUMBER_OF_ROUNDS: usize = 20;
+const NUMBER_OF_ROUNDS_1: usize = 20;
+const NUMBER_OF_ROUNDS_2: usize = 10000;
 
 fn main() -> io::Result<()> {
     let file = File::open("input")?;
@@ -12,40 +13,45 @@ fn main() -> io::Result<()> {
         .map(|l| l.unwrap().trim().to_string())
         .collect::<Vec<String>>();
     println!("Input: {:?}", input);
-    println!("Part 1: {}", part1(&input));
-    println!("Part 2: {}", part2(&input));
+    println!("Part 1: {}", part1(&mut read_monkeys(&input)));
+    println!("Part 2: {}", part2(&mut read_monkeys(&input)));
     Ok(())
 }
 
 struct Monkey {
     id: usize,
-    items: Vec<i64>,
-    operation: Box<dyn Fn(i64) -> i64>,
-    test: Box<dyn Fn(i64) -> usize>,
-    inspections: i64,
+    items: Vec<u64>,
+    operation: Box<dyn Fn(u64) -> u64>,
+    test: Box<dyn Fn(u64) -> usize>,
+    inspections: u64,
+    divisor: u64,
 }
 
 impl Monkey {
     fn new() -> Monkey {
         Monkey {
             id: 0,
-            items: Vec::<i64>::new(),
+            items: Vec::<u64>::new(),
             operation: Box::new(move |worry| worry),
             test: Box::new(move |worry| worry as usize),
             inspections: 0,
+            divisor: 0,
         }
     }
 
-    fn inspect_throw(&mut self, item: i64) -> (i64, usize) {
+    fn inspect_throw(&mut self, item: u64, lcm: Option<u64>) -> (u64, usize) {
         self.inspections += 1;
-        let worry = (self.operation)(item) / 3;
+        let worry = match lcm {
+            Some(x) => (self.operation)(item) % x,
+            None => (self.operation)(item) / 3,
+        };
         (worry, (self.test)(worry))
     }
 }
 
-fn part1(input: &Vec<String>) -> i64 {
+fn read_monkeys(input: &Vec<String>) -> Vec<Monkey> {
     let mut monkeys = Vec::<Monkey>::new();
-    let mut divisor = 1i64; // problably need to be a vector
+    let mut divisor = 1u64; 
     let mut m0 = 0usize;
     let mut m1 = 0usize;
     let mut count = 0usize;
@@ -61,8 +67,8 @@ fn part1(input: &Vec<String>) -> i64 {
             monkeys[count].items = line
                 .replace("Starting items: ", "")
                 .split(", ")
-                .map(|x| x.parse::<i64>().unwrap())
-                .collect::<Vec<i64>>();
+                .map(|x| x.parse::<u64>().unwrap())
+                .collect::<Vec<u64>>();
         } else if line.starts_with("Operation") {
             let op_string = line.replace("Operation: new = old ", "");
             let items = op_string.split(" ").collect::<Vec<&str>>();
@@ -71,7 +77,7 @@ fn part1(input: &Vec<String>) -> i64 {
                     monkeys[count].operation = Box::new(move |worry| worry * worry);
                     continue;
                 }
-                _ => items[1].parse::<i64>().unwrap(),
+                _ => items[1].parse::<u64>().unwrap(),
             };
             monkeys[count].operation = match items[0] {
                 "+" => Box::new(move |worry| worry + value),
@@ -80,7 +86,8 @@ fn part1(input: &Vec<String>) -> i64 {
             }
         } else if line.starts_with("Test") {
             let items = line.replace("Test: divisible by ", "");
-            divisor = items.parse::<i64>().unwrap();
+            divisor = items.parse::<u64>().unwrap();
+            monkeys[count].divisor = divisor;
         } else if line.starts_with("If true") {
             m0 = line
                 .replace("If true: throw to monkey ", "")
@@ -105,12 +112,15 @@ fn part1(input: &Vec<String>) -> i64 {
         }
     }
     monkeys.pop();
+    monkeys
+}
 
-    for r in 0..NUMBER_OF_ROUNDS {
+fn part1(monkeys: &mut Vec<Monkey>) -> u64 {
+    for _ in 0..NUMBER_OF_ROUNDS_1 {
         for index in 0..monkeys.len() {
             let mut monkey = monkeys.remove(index);
             while let Some(item) = monkey.items.pop() {
-                let (updated_item, next_monkey) = monkey.inspect_throw(item);
+                let (updated_item, next_monkey) = monkey.inspect_throw(item, None);
                 monkeys[next_monkey - {
                     if next_monkey > index {
                         1
@@ -128,6 +138,26 @@ fn part1(input: &Vec<String>) -> i64 {
     monkeys[0].inspections * monkeys[1].inspections
 }
 
-fn part2(input: &Vec<String>) -> i64 {
-    0
+fn part2(monkeys: &mut Vec<Monkey>) -> u64 {
+    let lcm = monkeys.iter().map(|m| m.divisor).fold(1, |acc, x| acc * x);
+    for _ in 0..NUMBER_OF_ROUNDS_2 {
+        for index in 0..monkeys.len() {
+            let mut monkey = monkeys.remove(index);
+            while let Some(item) = monkey.items.pop() {
+                let (updated_item, next_monkey) = monkey.inspect_throw(item, Some(lcm));
+                monkeys[next_monkey - {
+                    if next_monkey > index {
+                        1
+                    } else {
+                        0
+                    }
+                }]
+                .items
+                .push(updated_item);
+            }
+            monkeys.insert(index, monkey);
+        }
+    }
+    monkeys.sort_by(|a, b| b.inspections.cmp(&a.inspections));
+    monkeys[0].inspections * monkeys[1].inspections
 }
