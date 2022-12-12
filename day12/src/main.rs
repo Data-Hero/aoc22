ef1f95f44ac4abcaa58e163aae5badf1c3f374ca
Bijan Riesenberg <B.Riesenberg@gmx.de>
2022-12-12 18:21:01 +0100

    day12 part 2

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -21,40 +21,60 @@ struct Node {
 }
 
 enum Direction {
-    Up,
-    Down,
-    Left,
-    Right,
+    West,
+    East,
+    North,
+    South,
 }
 
-fn is_traversable(direction: Direction, x: usize, y: usize, input: &Vec<String>, nodes: &HashMap<(u8,u8), NodeIndex>, graph: &Graph<Node, i16, Directed>, cn: &NodeIndex) -> bool {
+fn is_traversable(
+    direction: Direction,
+    x: usize,
+    y: usize,
+    input: &Vec<String>,
+    nodes: &HashMap<(u8, u8), NodeIndex>,
+    graph: &Graph<Node, i16, Directed>,
+    cn: &NodeIndex,
+) -> bool {
     let max_x = input[0].len();
     let max_y = input.len();
     return match direction {
-        Direction::Left => x > 0 && graph
+        Direction::West => {
+            x > 0
+                && graph
                     .node_weight(*nodes.get(&(x as u8 - 1, y as u8)).unwrap())
                     .unwrap()
                     .height
                     - graph.node_weight(*cn).unwrap().height
-                    <= 1,
-        Direction::Right => x + 1 < max_x && graph
-                    .node_weight(*nodes.get(&(x as u8 + 1, y as u8)).unwrap())
-                    .unwrap()
-                    .height
-                    - graph.node_weight(*cn).unwrap().height
-                    <= 1,
-        Direction::Up => y > 0 && graph
+                    <= 1
+        }
+        Direction::North => {
+            y > 0
+                && graph
                     .node_weight(*nodes.get(&(x as u8, y as u8 - 1)).unwrap())
                     .unwrap()
                     .height
                     - graph.node_weight(*cn).unwrap().height
-                    <= 1,
-        Direction::Down => y + 1 < max_y && graph
+                    <= 1
+        }
+        Direction::East => {
+            x + 1 < max_x
+                && graph
+                    .node_weight(*nodes.get(&(x as u8 + 1, y as u8)).unwrap())
+                    .unwrap()
+                    .height
+                    - graph.node_weight(*cn).unwrap().height
+                    <= 1
+        }
+        Direction::South => {
+            y + 1 < max_y
+                && graph
                     .node_weight(*nodes.get(&(x as u8, y as u8 + 1)).unwrap())
                     .unwrap()
                     .height
                     - graph.node_weight(*cn).unwrap().height
-                    <= 1,
+                    <= 1
+        }
     };
 }
 
@@ -87,25 +107,80 @@ fn part1(input: &Vec<String>) -> i64 {
     for (li, line) in input.iter().enumerate() {
         for (bi, _) in line.as_bytes().iter().enumerate() {
             let cn = *nodes.get(&(bi as u8, li as u8)).unwrap();
-            if is_traversable(Direction::Up, bi, li, input, &nodes, &graph, &cn) {
-                graph.add_edge(cn, *nodes.get(&(bi as u8, li as u8 - 1)).unwrap(), 1);
-            }
-            if is_traversable(Direction::Down, bi, li, input, &nodes, &graph, &cn) {
-                graph.add_edge(cn, *nodes.get(&(bi as u8, li as u8 + 1)).unwrap(), 1);
-            }
-            if is_traversable(Direction::Left, bi, li, input, &nodes, &graph, &cn) {
+            if is_traversable(Direction::West, bi, li, input, &nodes, &graph, &cn) {
                 graph.add_edge(cn, *nodes.get(&(bi as u8 - 1, li as u8)).unwrap(), 1);
             }
-            if is_traversable(Direction::Right, bi, li, input, &nodes, &graph, &cn) {
+            if is_traversable(Direction::North, bi, li, input, &nodes, &graph, &cn) {
+                graph.add_edge(cn, *nodes.get(&(bi as u8, li as u8 - 1)).unwrap(), 1);
+            }
+            if is_traversable(Direction::East, bi, li, input, &nodes, &graph, &cn) {
+                graph.add_edge(cn, *nodes.get(&(bi as u8 + 1, li as u8)).unwrap(), 1);
+            }
+            if is_traversable(Direction::South, bi, li, input, &nodes, &graph, &cn) {
+                graph.add_edge(cn, *nodes.get(&(bi as u8, li as u8 + 1)).unwrap(), 1);
+            }
+        }
+    }
+
+    let result = dijkstra(&graph, start, Some(goal), |_| 1);
+    *result.iter().filter(|(k, _)| **k == goal).next().unwrap().1
+}
+
+fn part2(input: &Vec<String>) -> i64 {
+    let mut graph: Graph<Node, i16, Directed> = Graph::new();
+    let mut nodes: HashMap<(u8, u8), NodeIndex> = HashMap::new();
+    let mut starts: Vec<NodeIndex> = Vec::<NodeIndex>::new();
+    let mut goal: NodeIndex = NodeIndex::new(0);
+
+    for (li, line) in input.iter().enumerate() {
+        for (bi, byte) in line.as_bytes().iter().enumerate() {
+            let height = match byte {
+                b'S' => b'a',
+                b'E' => b'z',
+                x => *x,
+            };
+            let node_id = graph.add_node(Node {
+                height: height as i16,
+            });
+            nodes.insert((bi as u8, li as u8), node_id);
+            if *byte as char == 'S' || *byte as char == 'a' {
+                starts.push(node_id);
+            }
+            if *byte as char == 'E' {
+                goal = node_id;
+            }
+        }
+    }
+
+    for (li, line) in input.iter().enumerate() {
+        for (bi, _) in line.as_bytes().iter().enumerate() {
+            let cn = *nodes.get(&(bi as u8, li as u8)).unwrap();
+            if is_traversable(Direction::North, bi, li, input, &nodes, &graph, &cn) {
+                graph.add_edge(cn, *nodes.get(&(bi as u8, li as u8 - 1)).unwrap(), 1);
+            }
+            if is_traversable(Direction::South, bi, li, input, &nodes, &graph, &cn) {
+                graph.add_edge(cn, *nodes.get(&(bi as u8, li as u8 + 1)).unwrap(), 1);
+            }
+            if is_traversable(Direction::West, bi, li, input, &nodes, &graph, &cn) {
+                graph.add_edge(cn, *nodes.get(&(bi as u8 - 1, li as u8)).unwrap(), 1);
+            }
+            if is_traversable(Direction::East, bi, li, input, &nodes, &graph, &cn) {
                 graph.add_edge(cn, *nodes.get(&(bi as u8 + 1, li as u8)).unwrap(), 1);
             }
         }
     }
 
-    let res = dijkstra(&graph, start, Some(goal), |_| 1);
-    *res.iter().filter(|(k, _)| **k == goal).next().unwrap().1
-}
-
-fn part2(input: &Vec<String>) -> i64 {
-    0
+    let mut results = Vec::<i64>::new();
+    for start in starts {
+        let result = dijkstra(&graph, start, Some(goal), |_| 1);
+        results.push(
+            *result
+                .iter()
+                .filter(|(k, _)| **k == goal)
+                .next()
+                .unwrap_or((&NodeIndex::new(0), &i64::MAX))
+                .1,
+        );
+    }
+    *results.iter().min().unwrap()
 }
