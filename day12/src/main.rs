a08f8f76f9a8ebcb52cd4fa8125f1fb67f71b8c9
Bijan Riesenberg <B.Riesenberg@gmx.de>
2022-12-12 17:20:09 +0100

    day12 part1

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,10 +1,9 @@
-use std::fs::File;
-use std::io::{self, prelude::*, BufReader};
-use petgraph::Graph;
 use petgraph::algo::dijkstra;
 use petgraph::prelude::*;
+use petgraph::Graph;
 use std::collections::HashMap;
-
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
 
 fn main() -> io::Result<()> {
     let file = File::open("input")?;
@@ -13,74 +12,96 @@ fn main() -> io::Result<()> {
     println!("Input: {:?}", input);
     println!("Part 1: {}", part1(&input));
     println!("Part 2: {}", part2(&input));
-
-    let mut graph : Graph<(),(),Directed>= Graph::new();
-    let a = graph.add_node(()); // node with no weight
-    let b = graph.add_node(());
-    let c = graph.add_node(());
-    let d = graph.add_node(());
-    let e = graph.add_node(());
-    let f = graph.add_node(());
-    let g = graph.add_node(());
-    let h = graph.add_node(());
-    // z will be in another connected component
-    let z = graph.add_node(());
-    
-    graph.extend_with_edges(&[
-        (a, b),
-        (b, c),
-        (c, d),
-        (d, a),
-        (e, f),
-        (b, e),
-        (f, g),
-        (g, h),
-        (h, e)
-    ]);
-    // a ----> b ----> e ----> f
-    // ^       |       ^       |
-    // |       v       |       v
-    // d <---- c       h <---- g
-    
-    let expected_res: HashMap<NodeIndex, usize> = [
-         (a, 3),
-         (b, 0),
-         (c, 1),
-         (d, 2),
-         (e, 1),
-         (f, 2),
-         (g, 3),
-         (h, 4)
-        ].iter().cloned().collect();
-    let res = dijkstra(&graph,b,None, |_| 1);
-    assert_eq!(res, expected_res);
-    println!("Graph: {:?}", graph);
-    println!("Dijkstra: {:?}", res);
-
     Ok(())
 }
 
-
+#[derive(Debug)]
+struct Node {
+    height: i16,
+}
 
 
 fn part1(input: &Vec<String>) -> i64 {
-    let mut graph: Graph<u8, u8, Directed> = Graph::new();
-    let linelength = input[0].len();
+    let mut graph: Graph<Node, i16, Directed> = Graph::new();
+    let mut nodes: HashMap<(u8, u8), NodeIndex> = HashMap::new();
+    let mut start: NodeIndex = NodeIndex::new(0);
+    let mut goal: NodeIndex = NodeIndex::new(0);
+    let input_length = input.len();
+    let line_length = input[0].len();
 
-    for line in input {
-        for b in line.as_bytes() {
-            graph.add_node(*b);
-        }
-    }
-    println!("Graph: {:?}", graph);
     for (li, line) in input.iter().enumerate() {
         for (bi, byte) in line.as_bytes().iter().enumerate() {
-            if li - 1 > 0 {
-
+            let height = match byte {
+                b'S' => b'a',
+                b'E' => b'z',
+                x => *x,
+            };
+            let node_id = graph.add_node(Node {
+                height: height as i16,
+            });
+            nodes.insert((bi as u8, li as u8), node_id);
+            if *byte as char == 'S' {
+                println!("Start: {:?}, x: {}, y: {}", graph.node_weight(node_id).unwrap(), bi, li);
+                start = node_id;
+            }
+            if *byte as char == 'E' {
+                goal = node_id;
             }
         }
     }
-    0
+
+    for (li, line) in input.iter().enumerate() {
+        for (bi, _) in line.as_bytes().iter().enumerate() {
+            let a = *nodes.get(&(bi as u8, li as u8)).unwrap();
+            if li > 0 {
+                if graph
+                    .node_weight(*nodes.get(&(bi as u8, li as u8 - 1)).unwrap())
+                    .unwrap()
+                    .height
+                    - graph.node_weight(a).unwrap().height
+                    <= 1
+                {
+                    graph.add_edge(a, *nodes.get(&(bi as u8, li as u8 - 1)).unwrap(), 1);
+                }
+            }
+            if li + 1 < input_length {
+                if graph
+                    .node_weight(*nodes.get(&(bi as u8, li as u8 + 1)).unwrap())
+                    .unwrap()
+                    .height
+                    - graph.node_weight(a).unwrap().height
+                    <= 1
+                {
+                    graph.add_edge(a, *nodes.get(&(bi as u8, li as u8 + 1)).unwrap(), 1);
+                }
+            }
+            if bi > 0 {
+                if graph
+                    .node_weight(*nodes.get(&(bi as u8 - 1, li as u8)).unwrap())
+                    .unwrap()
+                    .height
+                    - graph.node_weight(a).unwrap().height
+                    <= 1
+                {
+                    graph.add_edge(a, *nodes.get(&(bi as u8 - 1, li as u8)).unwrap(), 1);
+                }
+            }
+            if bi + 1 < line_length {
+                if graph
+                    .node_weight(*nodes.get(&(bi as u8 + 1, li as u8)).unwrap())
+                    .unwrap()
+                    .height
+                    - graph.node_weight(a).unwrap().height
+                    <= 1
+                {
+                    graph.add_edge(a, *nodes.get(&(bi as u8 + 1, li as u8)).unwrap(), 1);
+                }
+            }
+        }
+    }
+
+    let res = dijkstra(&graph, start, Some(goal), |_| 1);
+    *res.iter().filter(|(k, _)| **k == goal).next().unwrap().1
 }
 
 fn part2(input: &Vec<String>) -> i64 {
